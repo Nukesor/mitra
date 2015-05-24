8ca60f8cd8d9aa754cf25c5e6b9b26d4222b9118
Arne Beer <arne@twobeer.de>
2015-05-24 23:30:37 +0200

    DB Relationships

--- mitra/models/category.py
+++ mitra/models/category.py
@@ -4,6 +4,9 @@ class Category(db.model):
     name = db.Column(db.String(120), primary_key=True)
     userid = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    entries = db.relationship('Entry', backref='category', lazy='dynamic')
+    monthlies = db.relationship('Monthly', backref='category', lazy='dynamic')
+
     def __init__(self, name, userid):
         self.name = name
         self.userid = userid
--- mitra/models/user.py
+++ mitra/models/user.py
@@ -5,6 +5,10 @@ class User(db.Model):
     username = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True)
 
+    categories = db.relationship('Category', backref='user', lazy='dynamic')
+    entries = db.relationship('Entry', backref='user', lazy='dynamic')
+    monthlies = db.relationship('Monthly', backref='user', lazy='dynamic')
+
     def __init__(self, id, username, email):
         self.id = id
         self.username = username
