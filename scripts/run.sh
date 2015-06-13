2f1dcfa2cbbc5fe857e891811ac3ac90885bc1d3
Arne Beer <arne@twobeer.de>
2015-06-13 08:51:32 +0200

    Flask and gulp fix, tmux refactoring

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-. venv/bin/activate
-python3 run.py
+tmux has-session -t flask
+if [ $? -eq 0 ]; then
+    tmux kill-session -t flask
+    tmux kill-session -t gulp
+    sleep 1
+fi
+
+tmux new -d -s flask
+tmux send -t flask . SPACE venv/bin/activate ENTER
+tmux send -t flask python3 SPACE run.py ENTER
+
+tmux new -d -s gulp
+tmux send -t gulp gulp ENTER
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm --needed python-pip python-virtualenv
+sudo pacman -S --noconfirm --needed python-pip python-virtualenv tmux
 sudo npm install -g gulp
 virtualenv venv --distribute
