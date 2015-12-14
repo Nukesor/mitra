b94c3d6d23fdd6bb7ad192579a9b0739a5be13e7
Arne Beer <arne@twobeer.de>
2015-12-15 00:23:18 +0100

    WIP

--- client/app.jsx
+++ client/app.jsx
@@ -13,7 +13,6 @@ class App extends React.Component {
     //    componentDidMount : function () {
     //        window.mitra.helper.loginCheck();
     //    }
-    //
     render () {
         return (
             <div className="mainContainer">
